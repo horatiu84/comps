b23d4c81f7541c3423708c4ca953fe5caf800569
horatiu84 <horatiul@yahoo.com>
2023-01-10 18:02:14 +0200

    first steps

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Button>Click here!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See deal</Button>
+      </div>
+      <div>
+        <Button>Hide adds!</Button>
+      </div>
+      <div>
+        <Button>Something ...</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;

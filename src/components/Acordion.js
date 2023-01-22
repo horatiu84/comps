f458555f9075fbe402b82ea7591f3075aacc50dc
horatiu84 <horatiul@yahoo.com>
2023-01-22 12:48:14 +0200

    adding accordeon component

--- src/components/Acordion.js
+++ src/components/Acordion.js
@@ -1,17 +1,42 @@
-const Acordion = ({items}) => {
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
-    const renderItems = items.map( (item) => {
-        return (
-            <div key={item.id}>
-                <div>{item.label}</div>
-                <div>{item.content}</div>
-            </div>
-        )
-    })
+const Acordion = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
-    return <div>
-        {renderItems}
-    </div>
-}
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  };
+
+  const renderItems = items.map((item, index) => {
+    const isExpended = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpended ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+
+        {isExpended && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div className="border-x border-t rounded">{renderItems}</div>;
+};
 
 export default Acordion;
